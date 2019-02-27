91bdd1ce7463fcdf93ee1b67fc72ab7c6264e995
John Knecht V <jjk1492@rit.edu>
2019-02-26 21:05:53 -0500

    Create Visitor.java

---
+++ src/Visitor.java
@@ -0,0 +1,16 @@
+public class Visitor {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private String userID;
+
+    public Visitor(String firstName, String lastName, String address, String phoneNumber, String userID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.userID = userID;
+    }
+}
