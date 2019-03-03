d6f61b8a7081acd47889d4785577f05f540e38fa
Ryan Gillie <rag4214@rit.edu>
2019-03-03 18:46:06 -0500

    Visit Class completed

--- src/model/Visit.java
+++ src/model/Visit.java
@@ -1,6 +1,6 @@
 package model;
 
-import model.Visitor;
+import java.util.Date;
 
 /**
  * Represents a visit, one is created each time a visitor enters the library. Tracks the time of the visit, the date, and who visited.
@@ -9,22 +9,50 @@ import model.Visitor;
  */
 public class Visit {
 
-    private long startTime;
+    private Date date = new Date();
+    private long startTime = System.currentTimeMillis();
     private long endTime;
-    private String date;
-    private Visitor visitor;
+
+    private String visitorId;
 
     /**
      * Constructor for a visit, endTime is initially set to -1 indicating it hasn't ended yet.
-     * @param startTime - Starting time of the visit
-     * @param date - Date of the visit
      * @param visitor - Who visited the library
      */
-    public Visit(long startTime, String date, Visitor visitor) {
-        this.endTime = -1;
+    public Visit(Visitor visitor) {
+        this.visitorId = visitor.getUserID();
+    }
 
-        this.startTime = startTime;
-        this.date = date;
-        this.visitor = visitor;
+    /**
+     * Get the date the visit started
+     * @return Date visit started
+     */
+    public Date getStartDate() {
+        return date;
+    }
+
+    /**
+     * Get the visitor ID
+     * @return visitorID
+     */
+    public String getVisitorId() {
+        return visitorId;
+    }
+
+    /**
+     * End the visit
+     */
+    public void endVisit() {
+        this.endTime = System.currentTimeMillis();
+    }
+
+    /**
+     * Find how long someone was in the library
+     * @return Int representation of minutes spent in library
+     */
+    public int getVisitTimeMinutes() {
+        if (endTime == 0L) return (int)((System.currentTimeMillis() - startTime) / 1000) / 60;
+
+        return (int)((endTime - startTime) / 1000) / 60;
     }
 }
