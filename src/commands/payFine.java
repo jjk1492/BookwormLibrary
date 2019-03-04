ecbc41045d758037c8817ee9ed517324abf5e588
Christian Alvarez <cea5984@g.rit.edu>
2019-03-03 22:40:17 -0500

    updated payfine with logic to pay visitor fines

--- src/commands/payFine.java
+++ src/commands/payFine.java
@@ -5,17 +5,20 @@ import model.Visitor;
 public class payFine extends command {
     @Override
     public String runCommand(String[] args) {
-
-        Visitor v = library.getVisitors().get(args[1]);
-        if(v == null){
-            return "pay,invalid-user-id;";
-        }else{
-            double d = Double.parseDouble(args[2]);
-            if(d < 0 || d > v.getFine()){
-                return "pay,invalid-amount," + d + "," + v.getFine();
+        if(args.length == 3) {
+            Visitor v = library.getVisitors().get(args[1]);
+            if (v == null) {
+                return "pay,invalid-user-id;";
+            } else {
+                double d = Double.parseDouble(args[2]);
+                if (d < 0 || d > v.getFine()) {
+                    return "pay,invalid-amount," + d + "," + v.getFine() + ";";
+                }
+                v.payFine(d);
+                return "pay,success," + v.getFine() + ";";
             }
-            v.payFine(d);
-            return "pay,success," + v.getFine();
+        }else{
+            return "pay,missing-parameters,{visitorID,amount};";
         }
     }
 }
