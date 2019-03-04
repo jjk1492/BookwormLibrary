08b49f026b562603a036f5a06a1fd65b5d3dafc3
Christian Alvarez <cea5984@g.rit.edu>
2019-03-03 20:11:13 -0500

    updated register command to work

--- src/commands/registerVisitor.java
+++ src/commands/registerVisitor.java
@@ -4,10 +4,9 @@ public class registerVisitor extends command{
     @Override
     public String runCommand(String[] args) {
         if(args.length == 5) {
-            library.registerUser(args[1], args[2], args[3], args[4], "1");
-            return "success";
+            return library.registerUser(args[1], args[2], args[3], args[4]);
         }else{
-            return "";
+            return "register,missing-parameters,{first name,last name,address, phone-number};";
         }
     }
 }
