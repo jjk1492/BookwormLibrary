13ba9a3951f1b109b9e9941270eecd6580e568a7
John Knecht V <jjk1492@rit.edu>
2019-03-31 19:56:46 -0400

    Update Service.java
    Took 48 minutes

--- src/controller/command/commands/Service.java
+++ src/controller/command/commands/Service.java
@@ -1,38 +1,28 @@
 package controller.command.commands;
 
-import controller.QueryHandler;
 import controller.command.Command;
+import model.BookStore;
+import model.BookwormLibrary;
+
 
 public class Service implements Command {
 
     //client ID,service,info-service;
-    private Long clientID;
-    private String service;
-    private boolean correctArgCount;
+    private String[] commandArgs;
 
-    public Service(String[] args) {
-        if(args.length == 3){
-            correctArgCount = true;
-            clientID = Long.parseLong(args[0]);
-            service = args[2];
-        }else {
-            correctArgCount = false;
-        }
+    public Service(String[] commandArgs) {
+        this.commandArgs = commandArgs;
     }
 
     @Override
     public void execute() {
-        if(correctArgCount){
-            if(service.equals("google") || service.equals("local")){
-                QueryHandler.getActiveClients().get(clientID).setService(service);
-                System.out.println(clientID + ",service," + service + ",success;");
-            }else {
-                System.out.println(clientID + ",service," + service + ",unsuccessful;");
-                System.out.println("Service can be either \"google\" or \"local\";");
-            }
-        }else {
-            System.out.println(clientID + ",service," + service + ",unsuccessful;");
-            System.out.println("Did you input the correct number of arguments?;");
+        String service = this.commandArgs[2];
+
+        if( service.equalsIgnoreCase("local")){
+            BookwormLibrary.getInstance().setCatalogue(BookStore.getInstance().loadLocal());
+        }else{
+            BookwormLibrary.getInstance().setCatalogue(BookStore.getInstance().loadAPI());
         }
+
     }
 }
