be25421fdf85f2f3948d97476ecf9a19a0da0440
Christian Alvarez <cea5984@g.rit.edu>
2019-03-31 18:07:01 -0400

    Added functionality to create account Added functionality to Login
    Fixed a bug that prevented time from loading
    Fixed a bug that caused null pointer exceptions

--- src/controller/command/commands/LogOut.java
+++ src/controller/command/commands/LogOut.java
@@ -1,14 +1,19 @@
 package controller.command.commands;
 
+import controller.QueryHandler;
 import controller.command.Command;
 
 public class LogOut implements Command {
 
-    public LogOut() {
+    private Long clientID;
+
+    public LogOut(String[] args) {
+        clientID = Long.parseLong(args[0]);
     }
 
     @Override
     public void execute() {
-
+        QueryHandler.getActiveClients().get(clientID).setVisitor(null);
+        System.out.println(clientID + ",logout,success;");
     }
 }
