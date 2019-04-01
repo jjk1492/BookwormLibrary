2eb3a3b202e5fdc422847dec2cc39bbdc457473b
Christian Alvarez <cea5984@g.rit.edu>
2019-03-31 20:00:17 -0400

    Added functionality to borrowBook Added functionality to returnBook

--- src/controller/command/undoableCommands/BorrowBook.java
+++ src/controller/command/undoableCommands/BorrowBook.java
@@ -1,61 +1,45 @@
 package controller.command.undoableCommands;
 
 import controller.command.UndoableCommand;
+import model.BookwormLibrary;
+
+import java.util.ArrayList;
 
 /**
  * Borrow a book Command class
  */
 public class BorrowBook implements UndoableCommand {
+
+    private Long clientID;
+    private Long visitorID;
+    private ArrayList<String> bookIDs;
+
     public BorrowBook(String[] args) {
+        clientID = Long.parseLong(args[0]);
+        visitorID = Long.parseLong(args[2]);
+        bookIDs = new ArrayList<>();
+        for(int i = 3; i < args.length; i++){
+            bookIDs.add(args[i]);
+        }
     }
 
     @Override
     public void execute() {
-
+        String response = BookwormLibrary.getInstance().checkOut(visitorID, bookIDs);
+        if(response.equals("max-checkout-exceeded")){
+            System.out.println(clientID + ",borrow,book-limit-exceeded;");
+        }else{
+            System.out.println(clientID + ",borrow," + response + ";");
+        }
     }
 
     @Override
     public void undo() {
-
+        BookwormLibrary.getInstance().checkIn(visitorID, bookIDs);
     }
 
     @Override
     public void redo() {
-
+        execute();
     }
-
-    /*@Override
-    public String runCommand(String[] args) {
-        if (args.length >= 3) {
-            ArrayList < String > ids = new ArrayList < > ();
-
-            for (int i = 2; i <= args.length - 1; i++) {
-                if (i == 2 && i == args.length - 1) {
-                    //only one book in the list
-                    String s = args[i];
-                    s = s.replace("{", "");
-                    s = s.replace("}", "");
-                    ids.add(s);
-                } else if (i == 2) {
-                    //first book in the list
-                    //remove "{"
-                    String s = args[i];
-                    s = s.replace("{", "");
-                    ids.add(s);
-                } else if (i == args.length - 1) {
-                    //last book in the list
-                    //remove "}"
-                    String s = args[i];
-                    s = s.replace("}", "");
-                    ids.add(s);
-                } else {
-                    //and ID
-                    ids.add(args[i]);
-                }
-            }
-
-            return "borrow," + library.checkOut(args[1], ids) + ";";
-        }
-        return "borrow,missing-parameters,{visitorID,{id}};";
-    }*/
 }
--- src/controller/command/undoableCommands/ReturnBook.java
+++ src/controller/command/undoableCommands/ReturnBook.java
@@ -1,22 +1,37 @@
 package controller.command.undoableCommands;
 
 import controller.command.UndoableCommand;
+import model.BookwormLibrary;
+
+import java.util.ArrayList;
 
 /**
  * Return a book to the library Command
  */
 public class ReturnBook implements UndoableCommand {
+
+    private Long clientID;
+    private Long visitorID;
+    private ArrayList<String> bookIDs;
+
     public ReturnBook(String[] args) {
+        clientID = Long.parseLong(args[0]);
+        visitorID = Long.parseLong(args[2]);
+        bookIDs = new ArrayList<>();
+        for(int i = 3; i < args.length; i++){
+            bookIDs.add(args[i]);
+        }
     }
 
     @Override
     public void execute() {
-
+        String response = BookwormLibrary.getInstance().checkIn(visitorID, bookIDs);
+        System.out.println(clientID + ",borrow," + response);
     }
 
     @Override
     public void undo() {
-
+        BookwormLibrary.getInstance().checkOut(visitorID, bookIDs);
     }
 
     @Override
